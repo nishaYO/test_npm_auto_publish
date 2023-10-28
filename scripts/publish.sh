75d5db60fc4abfea6950b5a8b18f4c872c8a0330
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-28 10:26:35 +0530

    Bumped version to 3.0.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,12 +4,12 @@
 version='3.0.0'
 
 # Update package.json version
-npm version $version
+npm version 3.0.0
 
 # Commit and tag the changes
 git add .
-git commit -m "Bumped version to $version"
-git tag -a v$version -m "Version $version"
+git commit -m "Bumped version to 3.0.0"
+git tag -a v3.0.0 -m "Version 3.0.0"
 git push origin main
 git push --tags
 
